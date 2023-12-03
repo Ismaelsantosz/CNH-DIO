7c8149cb3cd05dbe7ba2204e56811fbba44abe9f
Ismaelsantosz <ismaelsantosz2109@gmail.com>
2023-12-03 19:09:54 -0300

    Segundo commit

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 // Nome e XP
 
 let nomeHeroi = "Iron-man"
-let xpHeroi = 1000
+let xpHeroi = 10020021
+let nivelHeroi = ""
 
 // Classificador
 
+if(xpHeroi < 1000){
+    nivelHeroi = "Ferro"}
 
+if(xpHeroi >= 1001 && xpHeroi <= 2000){
+    nivelHeroi = "Bronze"}
+
+if(xpHeroi >= 2001 && xpHeroi <= 5000){
+    nivelHeroi = "Prata"} 
+
+if(xpHeroi >= 6001 && xpHeroi <= 7000){
+    nivelHeroi = "Ouro"}    
+
+if(xpHeroi >= 7001 && xpHeroi <= 8000){
+    nivelHeroi = "Platina"}
+       
+if(xpHeroi >= 8001 && xpHeroi <= 9000){
+    nivelHeroi = "Ascendete"}
+    
+if(xpHeroi >= 9001 && xpHeroi <= 10000){
+    nivelHeroi = "Imortal"}
+    
+if(xpHeroi >= 10001){
+    nivelHeroi = "Radiante"}    
+
+//Saída
+
+console.log("O Herói de nome " + nomeHeroi + "está no nível de " + nivelHeroi) 
